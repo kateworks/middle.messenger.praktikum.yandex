602dc69716913190d2418ccf04229da43a613a66
Ekaterina Pozhidaeva <kateworks@yandex.ru>
2022-06-14 14:32:05 +0300

    Add typescript and config files

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// import { renderDOM, registerComponent } from './core';
+// import Button from './components/Button/Button';
+
+// registerComponent(Button);
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   // const App = new LoginPage();
+//   // const App = new OnboardingPage({
+//   //   links: [
+//   //     {to: '#signup', text: 'signup'},
+//   //     {to: '#login', text: 'login'},
+//   //   ]
+//   // });
+
+//   // renderDOM(App);
+
+//   const button = new Button({ title: '111' });
+//   renderDOM(button);
+// });
+console.log("Hello");
